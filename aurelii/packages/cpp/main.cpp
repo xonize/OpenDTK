151dc34f29a8b5d2d35069c521a00e8fc4e4dc06
Westsi <76999267+Westsi@users.noreply.github.com>
2022-12-12 20:38:20 +0000

    added c and cpp code

---
+++ aurelii/packages/cpp/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    cout << "This is the C++ package for Aurelii. I am not very well versed in C++ so please if you can contribute to this.\n";
+}
