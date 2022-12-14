7fa90b497005ab2a6b5370ef2c14e5c0f0a5a0ab
Westsi <76999267+Westsi@users.noreply.github.com>
2022-12-14 08:55:00 +0000

    updated spec readme

--- aurelii/packages/python/setup.py
+++ aurelii/packages/python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 '''To build + upload run python -m build then twine upload dist/*'''
 
 setup(name='aurelii',
-      version='0.1.2.2',
+      version='0.1.2.4',
       description='Aurelii file format python package',
       long_description='Open source Aurelii file format python package by Xonize in OpenDTK',
       classifiers=[
--- aurelii/spec/README.md
+++ aurelii/spec/README.md
@@ -1,3 +1,9 @@
 # Aurelii Specification
 
-All Aurelii packages and related tools should follow this specification.
+All Aurelii packages and related tools should follow this specification. 
+ 
+## Aims and Goals of the project
+
+- Speed
+- Storage Size
+- Good Interface
